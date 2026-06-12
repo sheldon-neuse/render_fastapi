9df1df51647807309e201320a86e048ce0a0cf21
sheldon-neuse <24fi081@ms.dendai.ac.jp>
2026-06-12 11:57:39 +0900

    update

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-from typing import Optional
+# from typing import Optional
 
-from fastapi import FastAPI
+# from fastapi import FastAPI
 
-app = FastAPI()
+# app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Optional[str] = None):
+#     return {"item_id": item_id, "q": q}
 
 
 from typing import Optional
